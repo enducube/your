c79a03c5c2f53a180b43faeaed6aff76748ca3c2
enducube <lskull238@gmail.com>
2021-03-16 11:42:11 +1300

    starting to use flask_login for user logins

--- app.py
+++ app.py
@@ -2,18 +2,27 @@ import eventlet
 from flask import Flask, render_template, g
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
+import flask_login
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/stuff.db'
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 class User(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String)
     password = db.Column(db.String)
+    flask_login.UserMixin()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get
+
 
 @app.route("/")
 def index():
@@ -32,9 +41,9 @@ def disconnect():
 
 @socketio.on("click")
 def bruh():
-    g[0] += 1
     emit("reload",broadcast=True)
+
+
 if __name__ == '__main__':
     print("bar 127.0.0.1:5000/")
-    g.append(1)
     socketio.run(app)
